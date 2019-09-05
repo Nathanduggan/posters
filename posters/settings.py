533a13d3bf6bde8c8176bdf2788f9cffc880704f
Nathan Duggan <nduggan@fb.com>
2019-09-05 17:25:33 +0100

    Updated Settings.py

--- posters/settings.py
+++ posters/settings.py
@@ -86,12 +86,18 @@ WSGI_APPLICATION = 'posters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+    
+else:
+        print("Database URL not found. Using SQLite instead")
+        DATABASE = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
+
 
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
