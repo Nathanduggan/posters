1a830298c9004aa6ae6fde8341944b87953c3455
Nathan Duggan <nduggan@fb.com>
2019-09-06 13:55:09 +0100

    Updated Settings.py

--- posters/settings.py
+++ posters/settings.py
@@ -86,24 +86,17 @@ WSGI_APPLICATION = 'posters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
-    
-else:
-        print("Database URL not found. Using SQLite instead")
-        DATABASE = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config(default='postgres://
+                                                        user:pass@host/db')
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 
-# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+Password validation
+https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
