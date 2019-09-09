ef26e2ed7aebc203a887d5eae566e45440c55d73
Nathan Duggan <nduggan@fb.com>
2019-09-09 17:12:10 +0100

    Updated Settings.py

--- posters/settings.py
+++ posters/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import dj_database_url
 import os
 try:
      import env
@@ -86,24 +86,18 @@ WSGI_APPLICATION = 'posters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    
-else:
-        print("Database URL not found. Using SQLite instead")
-        DATABASE = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
-
-
-
-Password validation
-https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
