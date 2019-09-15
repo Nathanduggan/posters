f6444ba2e989f0c35b7b6daf76b3cc47cd83925d
Nathan Duggan <nduggan@fb.com>
2019-09-15 14:15:53 +0100

    Updated CSS

--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
-
 from django.shortcuts import render
 from products.models import Product
+from django.template.context_processors import media
 
 # Create your views here.
 def do_search(request):
